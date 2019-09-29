b65b280209e195213f2f32e8758a864fca8fb582
Warwick Stone <uozu.aho@gmail.com>
2019-09-29 14:36:49 +1000

    add e2e test for 2 auctions, update main to accept multiple auction args

--- src/test/java/aho/uozu/auctionsniper/ApplicationRunner.java
+++ src/test/java/aho/uozu/auctionsniper/ApplicationRunner.java
@@ -8,14 +8,12 @@ public class ApplicationRunner {
     public static final String SNIPER_XMPP_ID = "sniper@host.xmpp.internal/Auction";
 
     private AuctionSniperDriver driver;
-    private String itemId;
 
-    public void startBiddingIn(final FakeAuctionServer auction) {
-        itemId = auction.getItemId();
+    public void startBiddingIn(final FakeAuctionServer... auctions) {
         Thread thread = new Thread("Test Application") {
             @Override public void run() {
                 try {
-                    App.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                    App.main(arguments(auctions));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -26,11 +24,13 @@ public class ApplicationRunner {
         driver = new AuctionSniperDriver(1000);
         driver.hasTitle(MainWindow.APPLICATION_TITLE);
         driver.hasColumnTitles();
-        driver.showsSniperStatus(itemId, 0, 0, SnipersTableModel.textFor(SniperState.JOINING));
+        for (var auction: auctions) {
+            driver.showsSniperStatus(auction.getItemId(), 0, 0, SnipersTableModel.textFor(SniperState.JOINING));
+        }
     }
 
-    public void showsSniperHasLostAuction(int lastPrice, int lastBid) {
-        driver.showsSniperStatus(itemId, lastPrice, lastBid, SnipersTableModel.textFor(SniperState.LOST));
+    public void showsSniperHasLostAuction(FakeAuctionServer auction, int lastPrice, int lastBid) {
+        driver.showsSniperStatus(auction.getItemId(), lastPrice, lastBid, SnipersTableModel.textFor(SniperState.LOST));
     }
 
     public void stop() {
@@ -44,11 +44,22 @@ public class ApplicationRunner {
                 SnipersTableModel.textFor(SniperState.BIDDING));
     }
 
-    public void hasShownSniperIsWinning(int winningBid) {
-        driver.showsSniperStatus(itemId, winningBid, winningBid, SnipersTableModel.textFor(SniperState.WINNING));
+    public void hasShownSniperIsWinning(FakeAuctionServer auction, int winningBid) {
+        driver.showsSniperStatus(auction.getItemId(), winningBid, winningBid, SnipersTableModel.textFor(SniperState.WINNING));
     }
 
-    public void showsSniperHasWonAuction(int lastPrice) {
-        driver.showsSniperStatus(itemId, lastPrice, lastPrice, SnipersTableModel.textFor(SniperState.WON));
+    public void showsSniperHasWonAuction(FakeAuctionServer auction, int lastPrice) {
+        driver.showsSniperStatus(auction.getItemId(), lastPrice, lastPrice, SnipersTableModel.textFor(SniperState.WON));
+    }
+
+    protected static String[] arguments(FakeAuctionServer... auctions) {
+        String[] arguments = new String[auctions.length + 3];
+        arguments[0] = XMPP_HOSTNAME;
+        arguments[1] = SNIPER_ID;
+        arguments[2] = SNIPER_PASSWORD;
+        for (int i = 0; i < auctions.length; i++) {
+            arguments[i + 3] = auctions[i].getItemId();
+        }
+        return arguments;
     }
 }
--- src/test/java/aho/uozu/auctionsniper/tests/AuctionSniperEndToEndTest.java
+++ src/test/java/aho/uozu/auctionsniper/tests/AuctionSniperEndToEndTest.java
@@ -6,47 +6,70 @@ import org.junit.After;
 import org.junit.Test;
 
 public class AuctionSniperEndToEndTest {
-    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final FakeAuctionServer auction1 = new FakeAuctionServer("item-54321");
+    private final FakeAuctionServer auction2 = new FakeAuctionServer("item-65432");
     private final ApplicationRunner application = new ApplicationRunner();
 
     @Test
     public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
-        auction.startSellingItem();
-        application.startBiddingIn(auction);
-        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
-        auction.announceClosed();
-        application.showsSniperHasLostAuction(0, 0);
+        auction1.startSellingItem();
+        application.startBiddingIn(auction1);
+        auction1.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.announceClosed();
+        application.showsSniperHasLostAuction(auction1, 0, 0);
     }
 
     @Test
     public void sniperMakesAHigherBidButLoses() throws Exception {
-        auction.startSellingItem();
-        application.startBiddingIn(auction);
-        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
-        auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding(auction, 1000, 1098);
-        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-        auction.announceClosed();
-        application.showsSniperHasLostAuction(1000, 1098);
+        auction1.startSellingItem();
+        application.startBiddingIn(auction1);
+        auction1.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.reportPrice(1000, 98, "other bidder");
+        application.hasShownSniperIsBidding(auction1, 1000, 1098);
+        auction1.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.announceClosed();
+        application.showsSniperHasLostAuction(auction1, 1000, 1098);
     }
 
     @Test public void
     sniperWinsAnAuctionByBiddingHigher() throws Exception {
-        auction.startSellingItem();
-        application.startBiddingIn(auction);
-        auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
-        auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding(auction, 1000, 1098); // last price, last bid
-        auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
-        auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
-        application.hasShownSniperIsWinning(1098); // winning bid
-        auction.announceClosed();
-        application.showsSniperHasWonAuction(1098); // last price
+        auction1.startSellingItem();
+        application.startBiddingIn(auction1);
+        auction1.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.reportPrice(1000, 98, "other bidder");
+        application.hasShownSniperIsBidding(auction1, 1000, 1098); // last price, last bid
+        auction1.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        application.hasShownSniperIsWinning(auction1, 1098); // winning bid
+        auction1.announceClosed();
+        application.showsSniperHasWonAuction(auction1, 1098); // last price
     }
+
+    @Test public void
+    sniperBidsForMultipleItems() throws Exception {
+        auction1.startSellingItem();
+        auction2.startSellingItem();
+        application.startBiddingIn(auction1, auction2);
+        auction1.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.reportPrice(1000, 98, "other bidder");
+        auction1.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.reportPrice(500, 21, "other bidder");
+        auction2.hasReceivedBid(521, ApplicationRunner.SNIPER_XMPP_ID);
+        auction1.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
+        auction2.reportPrice(521, 22, ApplicationRunner.SNIPER_XMPP_ID);
+        application.hasShownSniperIsWinning(auction1, 1098);
+        application.hasShownSniperIsWinning(auction2, 521);
+        auction1.announceClosed();
+        auction2.announceClosed();
+        application.showsSniperHasWonAuction(auction1, 1098);
+        application.showsSniperHasWonAuction(auction2, 521);
+    }
+
 
     @After
     public void stopAuction() {
-        auction.stop();
+        auction1.stop();
     }
 
     @After
